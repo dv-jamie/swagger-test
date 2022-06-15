e6a220c3a5e4805d9f26150e13183854fc67999b
dv-jamie <sjm2506@gmail.com>
2022-06-15 19:47:21 +0900

    posts 폴더 생성

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+    constructor(private readonly postsService: PostsService) {}
+
+    @Post()
+    async createPost() {
+
+    }
+}
