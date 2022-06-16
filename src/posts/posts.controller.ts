7ec128a883e319ff6e708bc981258be8d49aa34e
dv-jamie <sjm2506@gmail.com>
2022-06-16 09:07:06 +0900

    ApiOperation 추가

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,47 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
+@ApiTags('Posts')
 export class PostsController {
     constructor(private readonly postsService: PostsService) {}
-
+    
+    @Get()
+    @ApiOperation({
+        summary: '모든 게시글 조회',
+        description: '모든 게시글을 불러온다.'
+    })
+    async getPosts() {}
+    
+    @Get(':id')
+    @ApiOperation({
+        summary: '하나의 게시글 조회',
+        description: '하나의 게시글을 불러온다.'
+    })
+    async getPostById() {}
+    
     @Post()
-    async createPost() {
-
-    }
+    @ApiOperation({
+        summary: '게시글 등록',
+        description: '게시글을 등록한다.'
+    })
+    async createPost(
+        @Param('user-id') userId: number,
+        @Body('content') content: string
+    ) {}
+    
+    @Patch(':id')
+    @ApiOperation({
+        summary: '게시글 수정',
+        description: '하나의 게시글을 수정한다.'
+    })
+    async updatePost() {}
+    
+    @Delete(':id')
+    @ApiOperation({
+        summary: '게시글 삭제',
+        description: '하나의 게시글을 삭제한다.'
+    })
+    async deletePost() {}
 }
